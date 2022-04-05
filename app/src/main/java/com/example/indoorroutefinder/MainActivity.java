592436692f454106c0a48929ce2d0f9a57f936c7
Himashi Rathnayake <nikeshalarathnayake19@gmail.com>
2022-04-05 16:37:52 +0530

    display route

--- app/src/main/java/com/example/indoorroutefinder/MainActivity.java
+++ app/src/main/java/com/example/indoorroutefinder/MainActivity.java
@@ -1,36 +1,31 @@
 package com.example.indoorroutefinder;
 
 import static com.mapbox.mapboxsdk.style.expressions.Expression.eq;
-import static com.mapbox.mapboxsdk.style.expressions.Expression.geometryType;
-import static com.mapbox.mapboxsdk.style.expressions.Expression.get;
-import static com.mapbox.mapboxsdk.style.expressions.Expression.literal;
-import static com.mapbox.mapboxsdk.style.expressions.Expression.properties;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.fillColor;
-import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.fillOpacity;
 import static com.mapbox.mapboxsdk.style.expressions.Expression.exponential;
+import static com.mapbox.mapboxsdk.style.expressions.Expression.get;
 import static com.mapbox.mapboxsdk.style.expressions.Expression.interpolate;
+import static com.mapbox.mapboxsdk.style.expressions.Expression.literal;
 import static com.mapbox.mapboxsdk.style.expressions.Expression.stop;
 import static com.mapbox.mapboxsdk.style.expressions.Expression.zoom;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineColor;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineOpacity;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineWidth;
 
-import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.net.wifi.ScanResult;
-import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.indoorroutefinder.utils.Nav.Navigation;
+import com.example.indoorroutefinder.utils.navigation.NavigationActivity;
 import com.example.indoorroutefinder.utils.poiSelection.POISelectionActivity;
 import com.example.indoorroutefinder.utils.poiSelection.PoiGeoJsonObject;
 import com.mapbox.geojson.Feature;
-import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
@@ -39,7 +34,6 @@ import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
-import com.mapbox.mapboxsdk.style.layers.FillLayer;
 import com.mapbox.mapboxsdk.style.layers.LineLayer;
 import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
 import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
@@ -48,29 +42,19 @@ import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, Style.OnStyleLoaded {
 
     private GeoJsonSource indoorBuildingSource;
-    private List<List<Point>> boundingBoxList;
     private MapView mapView;
     private MapboxMap mapboxMap;
     private Style loadedStyle;
-//    private View levelButtons;
-    private int currentLevel = 0;
-    private final String goeFileName = "convention_hall_lvl_0_Nav_1.geojson";
-
-    // private final String STYLE_URL = "https://tilesservices.webservices.infsoft.com/api/mapstyle/style/";
-    // private final String API_KEY = "8c97d7c6-0c3a-41de-b67a-fb7628efba79";
-    // private final String INITIAL_3D = "FALSE";
+    String goeFileName = "convention_hall_lvl_0_Nav_1.geojson";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
-//        TelemetryDefinition telemetry = Mapbox.getTelemetry();
-//        telemetry.setUserTelemetryRequestState(false);
         setContentView(R.layout.activity_main);
         mapView = findViewById(R.id.mapView);
         mapView.getMapAsync(this);
@@ -79,12 +63,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(@NonNull MapboxMap mapboxMap) {
         this.mapboxMap = mapboxMap;
-//        String styleUrl = STYLE_URL + API_KEY + "?config=3d:" + INITIAL_3D;
         this.mapboxMap.setStyle(Style.MAPBOX_STREETS, this);
-//        this.mapboxMap.getUiSettings().setAttributionEnabled(false);
-//        this.mapboxMap.getUiSettings().setLogoEnabled(false);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onStyleLoaded(@NonNull Style style) {
         this.loadedStyle = style;
@@ -94,17 +76,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         POISelectionActivity.loadPOIs(loadJsonFromAsset(goeFileName));
         this.loadedStyle.addSource(indoorBuildingSource);
         loadBuildingLayer(this.loadedStyle);
-        mapboxMap.addOnMapClickListener(new MapboxMap.OnMapClickListener() {
-            @Override
-            public boolean onMapClick(@NonNull LatLng point) {
-                Feature selectedFeature = POISelectionActivity.findSelectedFeature(mapboxMap, point);
-                PoiGeoJsonObject selectedPoi = POISelectionActivity.findClickedPoi(selectedFeature);
-                if(selectedFeature != null) {
-                    POISelectionActivity.removeMarkers(mapboxMap);
-                }
-                POISelectionActivity.createMarker(mapView, mapboxMap, loadedStyle,getResources(),selectedPoi, selectedFeature);
-                return true;
+        mapboxMap.addOnMapClickListener(point -> {
+            Feature selectedFeature = POISelectionActivity.findSelectedFeature(mapboxMap, point);
+            PoiGeoJsonObject selectedPoi = POISelectionActivity.findClickedPoi(selectedFeature);
+            if(selectedFeature != null) {
+                POISelectionActivity.removeMarkers(mapboxMap);
             }
+            POISelectionActivity.createMarker(mapView, mapboxMap, loadedStyle,getResources(),selectedPoi, selectedFeature);
+            return true;
         });
 
 //        mapboxMap.addOnCameraMoveListener(new MapboxMap.OnCameraMoveListener() {
@@ -145,43 +124,19 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 //            }
 //        });
 
-        routeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // DisplayRouteActivity.onCalcRouteClicked(API_KEY);
-                WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-                List<ScanResult> results = wifiManager.getScanResults();
-                // int level = getPowerPercentage(results.get(0).level);
-                Log.i("wifi", String.valueOf(results));
-            }
+        routeButton.setOnClickListener(view -> {
+            // DisplayRouteActivity.onCalcRouteClicked(API_KEY);
+//                WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+//                List<ScanResult> results = wifiManager.getScanResults();
+//                // int level = getPowerPercentage(results.get(0).level);
+//                Log.i("wifi", String.valueOf(results));
+            int source = 1, dest = 11;
+            NavigationActivity.getShortestPath(source, dest, mapboxMap);
         });
 
-        initButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setInitialCamera();
-            }
-        });
-        Navigation.initNav(loadJsonFromAsset(goeFileName));
+        initButton.setOnClickListener(view -> setInitialCamera());
+        NavigationActivity.initNav(loadJsonFromAsset(goeFileName));
     }
-
-//    private void hideLevelButton() {
-//        // When the user moves away from our bounding box region or zooms out far enough the floor level
-//        // buttons are faded out and hidden.
-//        AlphaAnimation animation = new AlphaAnimation(1.0f, 0.0f);
-//        animation.setDuration(500);
-//        levelButtons.startAnimation(animation);
-//        levelButtons.setVisibility(View.GONE);
-//    }
-//
-//    private void showLevelButton() {
-//        // When the user moves inside our bounding box region or zooms in to a high enough zoom level,
-//        // the floor level buttons are faded out and hidden.
-//        AlphaAnimation animation = new AlphaAnimation(0.0f, 1.0f);
-//        animation.setDuration(500);
-//        levelButtons.startAnimation(animation);
-//        levelButtons.setVisibility(View.VISIBLE);
-//    }
 
     private String loadJsonFromAsset(String filename) {
         // Using this method to load in GeoJSON files from the assets folder.
@@ -202,18 +157,25 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private void loadBuildingLayer(@NonNull Style style) {
         // Method used to load the indoor layer on the map. First the fill layer is drawn and then the
         // line layer is added.
-        FillLayer indoorBuildingLayer = new FillLayer("indoor-building-fill", "indoor-building").withProperties(
-                fillColor(Color.parseColor("#eeeeee")),
-                // Function.zoom is used here to fade out the indoor layer if zoom level is beyond 16. Only
-                // necessary to show the indoor map at high zoom levels.
-                fillOpacity(interpolate(exponential(1f), zoom(),
+//        FillLayer indoorBuildingLayer = new FillLayer("indoor-building-fill", "indoor-building").withProperties(
+//                fillColor(Color.parseColor("#eeeeee")),
+//                // Function.zoom is used here to fade out the indoor layer if zoom level is beyond 16. Only
+//                // necessary to show the indoor map at high zoom levels.
+//                fillOpacity(interpolate(exponential(0.2f), zoom(),
+//                        stop(16f, 0f),
+//                        stop(16.5f, 0.5f),
+//                        stop(17f, 1f)))
+//        );
+//
+//        style.addLayer(indoorBuildingLayer);
+
+        LineLayer indoorBuildingLineLayer = new LineLayer("indoor-building-line", "indoor-building").withProperties(
+                lineColor(Color.parseColor("#50667f")),
+                lineWidth(3f),
+                lineOpacity(interpolate(exponential(1f), zoom(),
                         stop(16f, 0f),
                         stop(16.5f, 0.5f),
                         stop(17f, 1f))));
-
-        style.addLayer(indoorBuildingLayer);
-
-        LineLayer indoorBuildingLineLayer = new LineLayer("indoor-building-line", "indoor-building");
         style.addLayer(indoorBuildingLineLayer);
 
         Bitmap icon = BitmapFactory.decodeResource(getResources(), R.drawable.location);
@@ -284,7 +246,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     @Override
-    public void onSaveInstanceState(Bundle outState) {
+    public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         if (mapView != null) {
             mapView.onSaveInstanceState(outState);
---
+++ app/src/main/java/com/example/indoorroutefinder/utils/navigation/NavigationActivity.java
@@ -0,0 +1,169 @@
+package com.example.indoorroutefinder.utils.navigation;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+
+import com.example.indoorroutefinder.utils.poiSelection.PoiGeoJsonObject;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mapbox.mapboxsdk.annotations.PolylineOptions;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class NavigationActivity {
+    private static Map<String, PoiGeoJsonObject> poiList = null;
+    private static final int verticesCount = 13;
+    private static final ArrayList<ArrayList<Integer>> edges = new ArrayList<>(verticesCount);
+
+
+    public static void initNav(String geoJsonSource) {
+        loadPOIs(geoJsonSource);
+
+        for (int i = 1; i < verticesCount; i++) {
+            edges.add(new ArrayList<>());
+        }
+
+        addEdge(1, 2);
+        addEdge(2, 3);
+        addEdge(3, 4);
+        addEdge(4, 5);
+        addEdge(5, 6);
+        addEdge(6, 7);
+        addEdge(6, 8);
+        addEdge(8, 9);
+        addEdge(8, 10);
+        addEdge(10, 11);
+    }
+
+    private static void loadPOIs(String geoJsonSource) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+
+        try {
+            Map<String, ArrayList<LinkedHashMap>> map = objectMapper.readValue(geoJsonSource, Map.class);
+            ArrayList<LinkedHashMap> pointers = map.get("features");
+            poiList = new HashMap<>();
+            for (LinkedHashMap pointer : pointers) {
+                LinkedHashMap geometry = (LinkedHashMap) pointer.get("geometry");
+                if (geometry.get("type").equals("Point")) {
+                    poiList.put((String) pointer.get("id"), new PoiGeoJsonObject((String) pointer.get("id"), (String) geometry.get("type"), (LinkedHashMap<String, String>) pointer.get("properties"), (ArrayList<String>) geometry.get("coordinates")));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void addEdge(int i, int j) {
+        edges.get(i).add(j);
+        edges.get(j).add(i);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public static void getShortestPath(int s, int dest, MapboxMap mapboxMap) {
+        // predecessor[i] array stores predecessor of
+        // i and distance array stores distance of i
+        // from s
+        int[] predecessor = new int[verticesCount];
+        int[] distance = new int[verticesCount];
+
+        if (!BFS(s, dest, predecessor, distance)) {
+            Log.i("Info", "Given source and destination are not connected");
+            return;
+        }
+
+        // LinkedList to store path
+        LinkedList<Integer> path = new LinkedList<>();
+        int crawl = dest;
+        path.add(crawl);
+        while (predecessor[crawl] != -1) {
+            path.add(predecessor[crawl]);
+            crawl = predecessor[crawl];
+        }
+
+        // Log distance
+        Log.i("Shortest path length is", String.valueOf(distance[dest]));
+
+        LinkedHashMap<String, ArrayList<String>> finalPath = new LinkedHashMap<>();
+        for (int i = path.size() - 1; i >= 0; i--) {
+            finalPath.put(String.valueOf(path.get(i)), null);
+        }
+        for (Map.Entry<String,PoiGeoJsonObject> entry : poiList.entrySet()){
+            String key = String.valueOf(entry.getValue().props.get("Nav"));
+            if(finalPath.containsKey(key)){
+                finalPath.put(key, entry.getValue().coordinates);
+            }
+        }
+        ArrayList<LatLng> points = finalPath.values().stream().map(point -> new LatLng(
+                Double.parseDouble(String.valueOf(point.get(1))),
+                Double.parseDouble(String.valueOf(point.get(0)))
+        )).collect(Collectors.toCollection(ArrayList::new));
+
+        // Log path
+        Log.i("Path is", String.valueOf(finalPath));
+        // add polyline to MapboxMap object
+        mapboxMap.addPolyline(new PolylineOptions()
+                .addAll(points)
+                .color(Color.RED)
+                .width(3f));
+    }
+
+    private static boolean BFS(int src, int dest, int[] predecessor, int[] dist) {
+        // a queue to maintain queue of vertices whose
+        // adjacency list is to be scanned as per normal
+        // BFS algorithm using LinkedList of Integer type
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        // boolean array visited[] which stores the
+        // information whether ith vertex is reached
+        // at least once in the Breadth first search
+        boolean[] visited = new boolean[verticesCount];
+
+        // initially all vertices are unvisited
+        // so v[i] for all i is false
+        // and as no path is yet constructed
+        // dist[i] for all i set to infinity
+        for (int i = 0; i < verticesCount; i++) {
+            visited[i] = false;
+            dist[i] = Integer.MAX_VALUE;
+            predecessor[i] = -1;
+        }
+
+        // now source is first to be visited and
+        // distance from source to itself should be 0
+        visited[src] = true;
+        dist[src] = 0;
+        queue.add(src);
+
+        // bfs Algorithm
+        while (!queue.isEmpty()) {
+            int u = queue.remove();
+            for (int i = 0; i < edges.get(u).size(); i++) {
+                if (!visited[edges.get(u).get(i)]) {
+                    visited[edges.get(u).get(i)] = true;
+                    dist[edges.get(u).get(i)] = dist[u] + 1;
+                    predecessor[edges.get(u).get(i)] = u;
+                    queue.add(edges.get(u).get(i));
+
+                    // stopping condition (when we find
+                    // our destination)
+                    if (edges.get(u).get(i) == dest)
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+
+}
