8eb6932d6b8a4e2a929f3e72d76488893effdac6
unknown <ra.kushan1997@gmail.com>
2022-04-11 00:07:42 +0530

    Bluetooth localization starts

--- app/src/main/java/com/example/indoorroutefinder/utils/poiSelection/POISelectionActivity.java
+++ app/src/main/java/com/example/indoorroutefinder/utils/poiSelection/POISelectionActivity.java
@@ -102,7 +102,10 @@ public class POISelectionActivity {
     }
 
     public static void userMarkRotate(double azimuth, SymbolManager symbolManager) {
-        userLoc.setIconRotate((float) azimuth);
-        symbolManager.update(userLoc);
+        if(symbolManager != null){
+            userLoc.setIconRotate((float) azimuth);
+            symbolManager.update(userLoc);
+        }
+
     }
 }
